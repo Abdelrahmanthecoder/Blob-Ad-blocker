dd8cd2488cfe38fa3a81db4f04d66db5260f6ecb
Abdelrahman <99421749+Abdelrahmanthecoder@users.noreply.github.com>
2022-12-31 23:02:39 +0300

    background.js code published

--- background.js
+++ background.js
@@ -1,1 +1,31 @@
+const defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+	"*://*.locanto.ca/*",
+	"*://*.oodle.com/*",
+	"*://*.classifiedads.com/*",
+	"*://*.adpost.com/*",
+	"*://*.gumtree.com/*",
+	"*://*.geebo.com/*",
+	"*://*.hoobly.com/*",
+	"*://*.yelp.com/*",
+	"*://*.olx.com/*",
+	"*://*.godaddy.com/*",
+	"*://*.hero-wars.com/*",
+	"*://*.blockchain-life.com/*",
+	"*://*.a-ads.com/*",
+]
 
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: defaultFilters },
+    ["blocking"]
+)
