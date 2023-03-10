30ceb471a9a9fec2fd1333952c8abb20038da8a3
Abdelrahman B <99421749+Abdelrahmanthecoder@users.noreply.github.com>
2023-03-10 07:28:36 +0300

    background.js updated

--- background.js
+++ background.js
@@ -1,26 +1,24 @@
-const adList = [
-    "*://*.doubleclick.net/*",
-    "*://*.googlesyndication.com/*",
-    "*://*.googletagservices.com/*",
-    "*://*.googleadservices.com/*",
-    "*://*.google-analytics.com/*",
-    "*://*.googleadservices.com/*",
-    "*://*.zedo.com/*",
-    "*://*.adbrite.com/*",
-    "*://*.adbureau.net/*",
-    "*://*.carbonads.net/*",
-    "*://*.cdn.carbonads.com/*",
-    "*://*.cdn.carbonads.net/*",
-    "*://*.cdn.doubleclick.net/*",
-    "*://*.cdn.googletagservices.com/*",
-    "*://*.cdn.googleadservices.com/*",
-    "*://*.cdn.zedo.com/*",
-];
-
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        return {cancel: true}
+        return {cancel: true};
     },
-    {urls: adList},
+    {urls: [
+        "*://*.doubleclick.net/*",
+        "*://*.googlesyndication.com/*",
+        "*://*.googletagservices.com/*",
+        "*://*.googleadservices.com/*",
+        "*://*.google-analytics.com/*",
+        "*://*.googleadservices.com/*",
+        "*://*.zedo.com/*",
+        "*://*.adbrite.com/*",
+        "*://*.adbureau.net/*",
+        "*://*.carbonads.net/*",
+        "*://*.cdn.carbonads.com/*",
+        "*://*.cdn.carbonads.net/*",
+        "*://*.cdn.doubleclick.net/*",
+        "*://*.cdn.googletagservices.com/*",
+        "*://*.cdn.googleadservices.com/*",
+        "*://*.cdn.zedo.com/*",
+    ]},
     ["blocking"]
-)
+);
