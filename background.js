85245d99662d3885f1506a0545c90d7db914829c
Abdelrahman B <99421749+Abdelrahmanthecoder@users.noreply.github.com>
2023-03-10 07:24:11 +0300

    background.js updated

--- background.js
+++ background.js
@@ -1,12 +1,26 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("BLOB Adblocker installed");
-  });
-  
+const adList = [
+    "*://*.doubleclick.net/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.googletagservices.com/*",
+    "*://*.googleadservices.com/*",
+    "*://*.google-analytics.com/*",
+    "*://*.googleadservices.com/*",
+    "*://*.zedo.com/*",
+    "*://*.adbrite.com/*",
+    "*://*.adbureau.net/*",
+    "*://*.carbonads.net/*",
+    "*://*.cdn.carbonads.com/*",
+    "*://*.cdn.carbonads.net/*",
+    "*://*.cdn.doubleclick.net/*",
+    "*://*.cdn.googletagservices.com/*",
+    "*://*.cdn.googleadservices.com/*",
+    "*://*.cdn.zedo.com/*",
+];
+
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        console.log("Blocking:", details.url);
-        return {cancel: true};
+        return {cancel: true}
     },
     {urls: adList},
     ["blocking"]
-);
+)
