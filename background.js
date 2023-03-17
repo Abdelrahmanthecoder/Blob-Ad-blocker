9725adb4ff90597d6e6d2c59f43406abe027c7dc
Abdelrahman B <99421749+Abdelrahmanthecoder@users.noreply.github.com>
2023-03-17 06:42:11 +0300

    background.js updated

--- background.js
+++ background.js
@@ -1,24 +1,30 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        return {cancel: true};
-    },
-    {urls: [
-        "*://*.doubleclick.net/*",
-        "*://*.googlesyndication.com/*",
-        "*://*.googletagservices.com/*",
-        "*://*.googleadservices.com/*",
-        "*://*.google-analytics.com/*",
-        "*://*.googleadservices.com/*",
-        "*://*.zedo.com/*",
-        "*://*.adbrite.com/*",
-        "*://*.adbureau.net/*",
-        "*://*.carbonads.net/*",
-        "*://*.cdn.carbonads.com/*",
-        "*://*.cdn.carbonads.net/*",
-        "*://*.cdn.doubleclick.net/*",
-        "*://*.cdn.googletagservices.com/*",
-        "*://*.cdn.googleadservices.com/*",
-        "*://*.cdn.zedo.com/*",
-    ]},
-    ["blocking"]
-); 
+chrome.declarativeNetRequest.onRequest.addRules({
+    id: "block_ads",
+    rules: [
+        {
+            action: {type: "block"},
+            condition: {
+                urls: [
+                    "*://*.doubleclick.net/*",
+                    "*://*.googlesyndication.com/*",
+                    "*://*.googletagservices.com/*",
+                    "*://*.googleadservices.com/*",
+                    "*://*.google-analytics.com/*",
+                    "*://*.googleadservices.com/*",
+                    "*://*.zedo.com/*",
+                    "*://*.adbrite.com/*",
+                    "*://*.adbureau.net/*",
+                    "*://*.carbonads.net/*",
+                    "*://*.cdn.carbonads.com/*",
+                    "*://*.cdn.carbonads.net/*",
+                    "*://*.cdn.doubleclick.net/*",
+                    "*://*.cdn.googletagservices.com/*",
+                    "*://*.cdn.googleadservices.com/*",
+                    "*://*.cdn.zedo.com/*",
+                ]
+            }
+        }
+    ]
+}, function() {
+    console.log("Blocking ads");
+});
