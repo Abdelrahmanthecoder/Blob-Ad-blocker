7defb3703caec31e6e005a8ae2ad68f43c131f11
Abdelrahman B <99421749+Abdelrahmanthecoder@users.noreply.github.com>
2023-03-27 05:40:59 +0300

    background.js updated

--- background.js
+++ background.js
@@ -1,30 +1,21 @@
-chrome.declarativeNetRequest.onRequest.addRules({
-    id: "block_ads",
-    rules: [
-        {
-            action: {type: "block"},
-            condition: {
-                urls: [
-                    "*://*.doubleclick.net/*",
-                    "*://*.googlesyndication.com/*",
-                    "*://*.googletagservices.com/*",
-                    "*://*.googleadservices.com/*",
-                    "*://*.google-analytics.com/*",
-                    "*://*.googleadservices.com/*",
-                    "*://*.zedo.com/*",
-                    "*://*.adbrite.com/*",
-                    "*://*.adbureau.net/*",
-                    "*://*.carbonads.net/*",
-                    "*://*.cdn.carbonads.com/*",
-                    "*://*.cdn.carbonads.net/*",
-                    "*://*.cdn.doubleclick.net/*",
-                    "*://*.cdn.googletagservices.com/*",
-                    "*://*.cdn.googleadservices.com/*",
-                    "*://*.cdn.zedo.com/*",
-                ]
-            }
-        }
-    ]
-}, function() {
-    console.log("Blocking ads");
-});
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+      // Check if request matches an ad network and block it if it does
+      if (isAd(details.url)) {
+        return { cancel: true };
+      }
+    },
+    { urls: ["<all_urls>"] },
+    ["blocking"]
+  );
+  
+  function isAd(url) {
+    // Check if URL matches an ad network
+    var adDomains = ["doubleclick.net", "googleadservices.com", "advertising.com"];
+    for (var i = 0; i < adDomains.length; i++) {
+      if (url.indexOf(adDomains[i]) !== -1) {
+        return true;
+      }
+    }
+    return false;
+}
