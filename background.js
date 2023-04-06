e5619bb546a3388dba93875ea5a3366383679f60
Abdelrahman B <99421749+Abdelrahmanthecoder@users.noreply.github.com>
2023-04-06 16:02:07 +0300

    background.js updated

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    // Check if request matches an ad network and block it if it does
     if (isAd(details.url)) {
       return { cancel: true };
     }
@@ -10,7 +9,6 @@ chrome.webRequest.onBeforeRequest.addListener(
 );
 
 function isAd(url) {
-  // Check if URL matches an ad network
   var adDomains = ["doubleclick.net", "googleadservices.com", "advertising.com"];
   for (var i = 0; i < adDomains.length; i++) {
     if (url.indexOf(adDomains[i]) !== -1) {
