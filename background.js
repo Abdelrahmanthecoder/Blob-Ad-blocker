47ee3ae4543ea2ca93bb4dd31e0ba81ac8f1042f
Abdelrahman B <99421749+Abdelrahmanthecoder@users.noreply.github.com>
2023-04-06 15:57:25 +0300

    background.s updated

--- background.js
+++ background.js
@@ -1,21 +1,21 @@
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-      // Check if request matches an ad network and block it if it does
-      if (isAd(details.url)) {
-        return { cancel: true };
-      }
-    },
-    { urls: ["<all_urls>"] },
-    ["blocking"]
-  );
-  
-  function isAd(url) {
-    // Check if URL matches an ad network
-    var adDomains = ["doubleclick.net", "googleadservices.com", "advertising.com"];
-    for (var i = 0; i < adDomains.length; i++) {
-      if (url.indexOf(adDomains[i]) !== -1) {
-        return true;
-      }
+  function(details) {
+    // Check if request matches an ad network and block it if it does
+    if (isAd(details.url)) {
+      return { cancel: true };
     }
-    return false;
+  },
+  { urls: ["<all_urls>"] },
+  ["blocking"]
+);
+
+function isAd(url) {
+  // Check if URL matches an ad network
+  var adDomains = ["doubleclick.net", "googleadservices.com", "advertising.com"];
+  for (var i = 0; i < adDomains.length; i++) {
+    if (url.indexOf(adDomains[i]) !== -1) {
+      return true;
+    }
+  }
+  return false;
 }
