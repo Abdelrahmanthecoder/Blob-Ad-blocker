44acd0b98d9fb0ed1b0b9cb33841b9e3f108fc74
Abdelrahman B <99421749+Abdelrahmanthecoder@users.noreply.github.com>
2023-04-28 14:49:32 +0300

    background.js updated

--- background.js
+++ background.js
@@ -1,18 +1,14 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: ["1"]
-  });
-  chrome.declarativeNetRequest.addRules([{
-    id: "1",
-    priority: 1,
-    action: {
-      type: "block"
-    },
-    condition: {
-      urlFilter: {
-        urls: ["<all_urls>"]
-      },
-      domains: ["doubleclick.net", "googleadservices.com", "advertising.com"]
-    }
-  }]);
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeNetRequest.updateDynamicRules({ removeRuleIds: ['1'] })
+    .then(() => {
+      chrome.declarativeNetRequest.addRules([{
+        id: '1',
+        priority: 1,
+        action: { type: 'block' },
+        condition: {
+          urlFilter: { urls: ['<all_urls>'] },
+          domains: ['doubleclick.net', 'googleadservices.com', 'advertising.com']
+        }
+      }]);
+    });
 });
