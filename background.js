60946905eaba72953eaf3a7c67c5f703bac0e35d
Abdelrahman B <99421749+Abdelrahmanthecoder@users.noreply.github.com>
2023-04-28 14:50:39 +0300

    background.js updated

--- background.js
+++ background.js
@@ -1,14 +1,16 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.declarativeNetRequest.updateDynamicRules({ removeRuleIds: ['1'] })
     .then(() => {
-      chrome.declarativeNetRequest.addRules([{
-        id: '1',
-        priority: 1,
-        action: { type: 'block' },
-        condition: {
-          urlFilter: { urls: ['<all_urls>'] },
-          domains: ['doubleclick.net', 'googleadservices.com', 'advertising.com']
-        }
-      }]);
+      chrome.declarativeNetRequest.setRules({
+        addRules: [{
+          id: '1',
+          priority: 1,
+          action: { type: 'block' },
+          condition: {
+            urlFilter: { urls: ['<all_urls>'] },
+            domains: ['doubleclick.net', 'googleadservices.com', 'advertising.com']
+          }
+        }]
+      });
     });
 });
